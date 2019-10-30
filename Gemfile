3b20d84b53ae181f8b4ea2db1812c645b3929b8a
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-30 16:07:14 +0000

    Adding the validation for friendship model

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'jquery-rails'
 gem 'devise-bootstrap-views'
 gem 'jquery-rails'
 gem 'twitter-bootstrap-rails'
+gem 'hirb'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,15 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  validate :users_are_not_already_friends
+
 
   # validates :confirmed, presence: true
+
+  def users_are_not_already_friends
+    if Friendship.where(user_id: friend_id, friend_id: user_id).exists? || Friendship.where(user_id: user_id, friend_id: friend_id).exists?
+      self.errors.add(:user_id, 'Already friends!')
+    end
+  end
+
 end
