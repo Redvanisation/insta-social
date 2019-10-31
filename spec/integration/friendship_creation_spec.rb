24ae094217f899bcc8d16a72105f231a3358986b
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-10-31 14:50:14 +0000

    Fixing the integration test for adding a friend

--- spec/integration/friendship_creation_spec.rb
+++ spec/integration/friendship_creation_spec.rb
@@ -3,27 +3,34 @@ require 'rails_helper'
 feature 'Friendship creation' do 
     let(:user) { create(:user) }
     let(:user2) { create(:user) }
-    let(:user3) { create(:user) }
-
     scenario 'Logging and making a friend request' do 
 
-      login user
+    login user
+    click_on 'Logout'
+    login user2
 
-      if user != current_user
-        click_on 'Add as a friend'
-      end
-      # visit '/'
-      # expect{
-      # click_button "Add as a friend"
-      # }.to change(Friendship, :count).by(1)
-
-      # expect(page).to have_content(user2.username)
-      # expect(page).to have_content(user3.username)
-
-      # expect do
-      #   click_button 'Hello'
-      # end.to change(Friendship, :count).by(1)
-      # expect(page).to have_button('commit')
+      expect do
+        click_button 'Add as a friend'
+      end.to change(Friendship, :count).by(1)
     end 
 
+
+    scenario 'Accept friendship request' do 
+
+        login user
+        click_on 'Logout'
+        login user2
+        click_on 'Add as a friend'
+         click_on 'Logout'
+      expect(page). to have_content("Log in")
+         login user 
+         expect(page). to have_content("Log in")
+        visit user_path(user)
+        expect(page). to have_content("Pending requests")
+       # click_button 'Accept'
+        #  expect(Friendship.last).to be(true)
+        end 
+    
 end 
+
+
