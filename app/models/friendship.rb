23813cf8d6ccd07c3391b881568682123bf0772b
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-11-01 09:43:24 +0000

    Add validation for the friendship request

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,11 +3,9 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
-  # validate :users_are_not_already_friends
+  validate :users_are_not_already_friends
 
-  # validates :confirmed, presence: true
-
-  # def users_are_not_already_friends
-  #   errors.add(:user_id, 'Already friends!') if Friendship.where(user_id: friend_id, friend_id: user_id).exists? || Friendship.where(user_id: user_id, friend_id: friend_id).exists?
-  # end
+  def users_are_not_already_friends
+    errors.add(:user_id, 'Already friends!') if Friendship.where(user_id: friend_id, friend_id: user_id).exists? && Friendship.where(user_id: user_id, friend_id: friend_id).exists?
+  end
 end
