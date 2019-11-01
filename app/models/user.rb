187e70b563c241a6c8a3a7434cec6aba3898201b
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-11-01 10:10:59 +0000

    Starting the milestone7

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   include UsersHelper
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook]
   has_many :posts
   has_many :comments
   has_many :likes
