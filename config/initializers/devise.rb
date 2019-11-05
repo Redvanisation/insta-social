a066bcd9eae52c8d22fafa6e7d7295581d9f57d9
Redvan <redvanisation@gmail.com>
2019-11-04 21:31:43 -0500

    setting up env variables before deploying

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,5 +297,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :facebook, '2426000817621711', '359ff024de4101ebe1c51ec0ce80a228'
+  config.omniauth :facebook, 'APP_ID', 'APP_SECRET'
 end
