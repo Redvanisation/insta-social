53001102ab2ad6bb3b5baad19c9e3412ac216f4f
Redvan <redvanisation@gmail.com>
2020-02-26 23:05:03 -0500

    fix posts index page

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const articleShowcase = document.querySelector('.article-showcase');
+
+  articleShowcase.addEventListener('click', () => {
+    alert('boo')
+  })
+})
