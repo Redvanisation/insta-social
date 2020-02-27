1feec22b917953db4e3aa16183bba25c31c0c064
Redvan <redvanisation@gmail.com>
2020-02-26 23:30:08 -0500

    Style posts index page

--- app/assets/javascripts/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const articleShowcase = document.querySelector('.article-showcase');
-
-  articleShowcase.addEventListener('click', () => {
-    alert('boo')
-  })
-})
